fe0c77d15548d2aa7050a8d567ebfa0b5319a741
Víctor J. Fernández <victor.fernandezgil@educa.madrid.org>
2019-02-01 12:27:12 +0100

    Inicial

---
+++ index.php
@@ -0,0 +1,11 @@
+<?php
+$titulo="Índice";
+include 'cabecera.inc';
+?>
+    <h2>Welcome to mai peich</h2>
+    <p>
+    Esta es la página principal de mi sitio de prueba
+    </p>
+<?php
+include 'pie.inc';
+?>                
